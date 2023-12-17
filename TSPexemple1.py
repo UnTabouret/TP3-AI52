bc754ba256b53092a50b6c7dc1b97f314bde3d8e
UnTabouret <jossua.guinot@gmail.com>
2023-12-17 22:50:15 +0100

    Finished graphing

--- TSPexemple1.py
+++ TSPexemple1.py
@@ -3,12 +3,14 @@
 import random
 import numpy as np
 import tkinter as tk
-import PIL
+import matplotlib.pyplot as plt
+import time
 
 # Données du problème (générées aléatoirement)
-NOMBRE_DE_VILLES = 10
-LARGEUR = 1000
-HAUTEUR = 1000
+ITERATIONS = 100
+NOMBRE_DE_VILLES = 20
+LARGEUR = 750
+HAUTEUR = 500
 MAX_DISTANCE = np.sqrt(pow(LARGEUR,2)+pow(HAUTEUR,2))
 positionsVilles = []
 for ville in range(NOMBRE_DE_VILLES):
@@ -42,6 +44,8 @@ def voisinage(solution,NOMBRE_DE_VILLES):
  
     
 # recuit simulé
+start_recuit = time.time()
+meilleurs_recuit = []
 solution=random.sample(range(NOMBRE_DE_VILLES),NOMBRE_DE_VILLES)
 cout0=cal_distance(solution,distances,NOMBRE_DE_VILLES)
 T=1000
@@ -49,8 +53,9 @@ facteur=0.99
 T_intiale=MAX_DISTANCE/2
 min_sol=solution
 cout_min_sol=cout0
-for i in range(100):
+for i in range(ITERATIONS):
     print('la ',i,'�me solution = ',solution,' donne la distance totale= ',cout0,' la temp�rature actuelle =',T)
+    meilleurs_recuit.append(cout0)
     T=T*facteur
     for j in range(50):
         nouv_sol=voisinage(solution*1,NOMBRE_DE_VILLES)
@@ -67,7 +72,8 @@ for i in range(100):
             if x<np.exp((cout0-cout1)/T):
                 cout0=cout1
                 solution=nouv_sol
-
+end_recuit = time.time()
+time_recuit = end_recuit-start_recuit
 print('voici la solution retenue ',min_sol,' et son coût ', cal_distance(min_sol,distances,NOMBRE_DE_VILLES))
 
 def croisementSinglePoint(chromosome1,chromosome2):
@@ -89,34 +95,15 @@ def mutation(chromosome):
     fils[source],fils[destination]= fils[destination],fils[source]  
     return fils
 
-#algo génétique
-baseChromosome = list(range(NOMBRE_DE_VILLES))
-TAILLE_POPULATION_GENETIQUE = 10
-populationGenetique = []
-for i in range(TAILLE_POPULATION_GENETIQUE):
-    random.shuffle(baseChromosome)
-    newChromosome = baseChromosome.copy()
-    populationGenetique.append(newChromosome)
-print("Population initiale génétique : ",populationGenetique)
-ITERATIONS_GENETIQUE = 1000
-for i in range(ITERATIONS_GENETIQUE):
-    populationGenetique.sort(key = lambda x: cal_distance(x,distances,NOMBRE_DE_VILLES))
-    populationGenetique = populationGenetique[:TAILLE_POPULATION_GENETIQUE-1]
-    for i in range(TAILLE_POPULATION_GENETIQUE):
-        populationGenetique.append(mutation(populationGenetique[i]))
-        populationGenetique.append(croisementSinglePoint(populationGenetique[i],populationGenetique[(i+1)%TAILLE_POPULATION_GENETIQUE]))
-
-for individu in populationGenetique:
-    print("Path : ",individu, " with distance : ",cal_distance(individu,distances,NOMBRE_DE_VILLES))
-
 #colonie de fourmis
-ALPHA = 0.5
-BETA = 0.5
+start_fourmis = time.time()
+meilleurs_fourmis = []
+ALPHA = 0.9
+BETA = 0.4
 Q = 1
-RHO = 0.7
+RHO = 0.1   
 THETA_ZERO = .001
-TAILLE_POPULATION_FOURMIS = 100
-ITERATIONS_FOURMIS = 100
+TAILLE_POPULATION_FOURMIS = 100 
 
 trace = np.zeros((NOMBRE_DE_VILLES,NOMBRE_DE_VILLES))
 deltaTrace = trace.copy()
@@ -146,7 +133,7 @@ def evaporation():
             element *= (1-RHO)
 
 
-for i in range(ITERATIONS_FOURMIS):
+for i in range(ITERATIONS):
     populationFourmis = [[] for i in range(TAILLE_POPULATION_FOURMIS)]
     for fourmi in populationFourmis:
         fourmi.append(random.randint(0,NOMBRE_DE_VILLES-1))
@@ -156,7 +143,39 @@ for i in range(ITERATIONS_FOURMIS):
     evaporation()
     trace += deltaTrace
     deltaTrace = np.zeros((NOMBRE_DE_VILLES,NOMBRE_DE_VILLES))
+    populationFourmis.sort(key = lambda x: cal_distance(x,distances,NOMBRE_DE_VILLES))
+    meilleurs_fourmis.append(cal_distance(populationFourmis[0],distances,NOMBRE_DE_VILLES))
 
+end_fourmis = time.time()
+time_fourmis = end_fourmis-start_fourmis
+
+
+
+#algo génétique
+start_genetique = time.time()
+meilleurs_genetique = []
+baseChromosome = list(range(NOMBRE_DE_VILLES))
+TAILLE_POPULATION_GENETIQUE = 500
+populationGenetique = []
+for i in range(TAILLE_POPULATION_GENETIQUE):
+    random.shuffle(baseChromosome)
+    newChromosome = baseChromosome.copy()
+    populationGenetique.append(newChromosome)
+print("Population initiale génétique : ",populationGenetique)
+
+for i in range(ITERATIONS):
+    populationGenetique.sort(key = lambda x: cal_distance(x,distances,NOMBRE_DE_VILLES))
+    meilleurs_genetique.append(cal_distance(populationGenetique[0],distances,NOMBRE_DE_VILLES))
+    populationGenetique = populationGenetique[:TAILLE_POPULATION_GENETIQUE-1]
+    for i in range(TAILLE_POPULATION_GENETIQUE):
+        populationGenetique.append(mutation(populationGenetique[i]))
+        populationGenetique.append(croisementSinglePoint(populationGenetique[i],populationGenetique[(i+1)%TAILLE_POPULATION_GENETIQUE]))
+
+end_genetique = time.time()
+time_genetique = end_genetique-start_genetique
+
+for individu in populationGenetique:
+    print("Path : ",individu, " with distance : ",cal_distance(individu,distances,NOMBRE_DE_VILLES))
 
 
 def create_circle(x, y, r, canvas): #center coordinates, radius
@@ -166,11 +185,11 @@ def create_circle(x, y, r, canvas): #center coordinates, radius
     y1 = y + r
     return canvas.create_oval(x0, y0, x1, y1,fill = "white")
 
-def tracerChemin(etapes,canvas):
+def tracerChemin(etapes,canvas,color="black",width=1):
     for i in range(1,len(etapes)):
         positionVille1 = positionsVilles[etapes[i-1]]
         positionVille2 = positionsVilles[etapes[i]]
-        canvas.create_line(positionVille1[1],positionVille1[0],positionVille2[1],positionVille2[0],)
+        canvas.create_line(positionVille1[1],positionVille1[0],positionVille2[1],positionVille2[0],fill=color,width = width)
 
 top = tk.Tk()
 graph = tk.Canvas(top, bg="white", height=HAUTEUR, width=LARGEUR)
@@ -178,7 +197,9 @@ graph.pack()
 
 
 #Dessin chemin
-tracerChemin(populationFourmis[0],graph)
+tracerChemin(min_sol,graph,"red",15)
+tracerChemin(populationGenetique[0],graph,"green",10)
+tracerChemin(populationFourmis[0],graph,"blue",5)
 
 #Dessin villes
 i=0
@@ -187,6 +208,14 @@ for ville in positionsVilles:
     graph.create_text(ville[1],ville[0],text=i)
     i+=1
 
+print("Temps pour recuit, genetique, fourmis : ",time_recuit,time_genetique,time_fourmis)
+
+#Plotting
+xpoints = np.array(range(ITERATIONS))
+plt.plot(xpoints,np.array(meilleurs_recuit),'r')
+plt.plot(xpoints,np.array(meilleurs_genetique),'g')
+plt.plot(xpoints,np.array(meilleurs_fourmis),'b')
+plt.show()
 
 
 graph.pack()
